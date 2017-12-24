99dcdc1be82d74b7f7837cd9308c2785679642f0
Librazy <im@librazy.org>
2017-12-25 05:17:34 +0800

    fix

--- HighGradeExtensions.cs
+++ HighGradeExtensions.cs
@@ -5,10 +5,10 @@ namespace Microsoft.Extensions.DependencyInjection
 {
     public static class InsomniaExtensions
     {
-        public static IServiceCollection AddHighGradeSeminarGroupService(this IServiceCollection serviceCollection) =>
+        public static IServiceCollection AddHighGradeSeminarService(this IServiceCollection serviceCollection) =>
             serviceCollection.AddScoped<ISeminarService, SeminarService>();
 
-        public static IServiceCollection AddHighGradeFixedGroupService(this IServiceCollection serviceCollection) =>
+        public static IServiceCollection AddHighGradeSchoolService(this IServiceCollection serviceCollection) =>
             serviceCollection.AddScoped<ISchoolService, SchoolService>();
 
         public static IServiceCollection AddHighGradeLoginService(this IServiceCollection serviceCollection) =>
